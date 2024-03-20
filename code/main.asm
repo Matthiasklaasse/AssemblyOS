a6e5627735b0d097d5faed4131f4eb5a7497839f
matthias klaassse <matthiasklaasse@gmail.com>
2024-03-20 21:28:19 +0100

    NOW WE HAVE COLORS

--- code/main.asm
+++ code/main.asm
@@ -1,5 +1,8 @@
 	BITS 16
-
+	text_string db 'This is my cool OS!', 0xd, 0xA
+	text_string1 db 'This is my cool line of code!', 0xd, 0xA
+	color db 0x0
+	
 start:
     mov     ax, 0x0012  ; maakt de bios 16-kleur mode
     int     10h
@@ -8,26 +11,24 @@ start:
     add ax, 288 
 	mov ss, ax
 	mov sp, 4096
+	; dit is de stack space vanaf 0x07C0 tot 0x07C0+288 zit de romvoor de bootloader
 
 	mov ax, 0x07C0
 	mov ds, ax; veel te moeilijk ben nu al vergeten hoe dit werkte was iets met een stack space LIFO etc.
+	; edit: dit blijkt de stack space te zijn hier slaan we variables zoals [color] in op vanaf 0x07C0 tot 288 verder zit dit in de ram
 
 
-	mov byte [2048h], 14
+	mov byte [color], 14
 	mov si, text_string ;in si drop je de HEX locatie van de 1ste byte je sting
 	call print_string	; print_string print de str die in si aangewezen word
+	mov byte [color], 14
 	mov si, text_string1
 	call print_string
 
 	jmp $
 
-	text_string db 'This is my cool OS!', 0xd, 0xA
-	text_string1 db 'This is my cool line of code!', 0xd, 0xA
-
-
 print_string:
-	mov esi, 0x2048 		; laad het adres van de kleur in de registers
-	mov bl, [esi]		;laad de kleul in bl
+	mov bl, [color]	;laad de kleur van 0x2048 in bl
 	mov ah, 0x0E	    ; ah op 0Eh zetten zet BIOS in text mode
 
 .print_string_repeat:
